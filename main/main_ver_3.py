d01389e6ed0f212a289a3a4be8a7ca3844780cea
Kohara <1k8aioon@gmail.com>
2023-04-27 16:06:22 +0900

    commit

--- main/main_ver_3.py
+++ main/main_ver_3.py
@@ -310,6 +310,7 @@ class Application(tk.Frame):
                     
                     if var_value == 1:
                         self.turn()# check!!
+                        
                     check = subprocess.getoutput("i2cget -y 1 0x40 0x02 w") #checkに値を入れる
                     if check == "Error: Read failed": #たまにエラーが起きるのでエラーの文字を受け取ったとき
                         res = b_res #前回の値を代入
